39aaa57756ed31117597908885c14204c39a5c78
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2021-07-25 19:45:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyrogram import Client as Bot
 
 from callsmusic import run
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import API_ID, API_HASH, BOT_TOKEN, BOT_USERNAME
 
 
 bot = Bot(
@@ -9,6 +9,7 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
+    bot_username=BOT_USERNAME
     plugins=dict(root="handlers")
 )
 
